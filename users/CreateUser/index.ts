f5c484c29f2772d1240d2d0d8b22b956e05e7d58
Jacob Marshall <github@jacob.dev>
2021-07-05 10:55:55 +1200

    Remove “functions” folder redundency

--- users/CreateUser/index.ts
+++ users/CreateUser/index.ts
@@ -1,6 +1,52 @@
 import { Context, HttpRequest } from '@azure/functions';
-import { createUser } from '../core/functions/createUser';
+import * as bcrypt from 'bcryptjs';
+import { getDatabase } from '../core/getDatabase';
+import { User } from '../core/models/User';
+import * as responses from '../core/responses/createUser';
+import { withoutResource } from '../core/models/withoutResource';
+
+interface CreateUser {
+  email: string;
+  password: string;
+}
 
 export default async function (context: Context, req: HttpRequest) {
-  context.res = await createUser(req.body);
+  const body: CreateUser = req.body;
+
+  if (typeof body.email !== 'string') {
+    return responses.MISSING_EMAIL_ADDRESS();
+  }
+  if (!body.email.includes('@')) {
+    return responses.INVALID_EMAIL_ADDRESS();
+  }
+  if (typeof body.password !== 'string') {
+    return responses.MISSING_PASSWORD();
+  }
+  if (body.password.length < 8) {
+    return responses.PASSWORD_TOO_SHORT();
+  }
+
+  const { users } = await getDatabase();
+
+  try {
+    const { resource: user } = await users.items.create<User>({
+      email: body.email.toLowerCase(),
+      password: await bcrypt.hash(body.password, 12),
+    });
+
+    return responses.USER_CREATED({
+      user: withoutResource(user),
+    });
+  } catch (err) {
+    console.error(err);
+
+    switch (err.code) {
+      case 409: {
+        return responses.USER_ALREADY_EXISTS();
+      }
+      default: {
+        return responses.FAILED_TO_CREATE_USER();
+      }
+    }
+  }
 };
--- users/Login/index.ts
+++ users/Login/index.ts
@@ -1,6 +1,64 @@
 import { Context, HttpRequest } from '@azure/functions';
-import { login } from '../core/functions/login';
+import { getDatabase } from '../core/getDatabase';
+import { User } from '../core/models/User';
+import * as bcrypt from 'bcryptjs';
+import * as responses from '../core/responses/login';
+import { withoutResource } from '../core/models/withoutResource';
+import { Resource } from '@azure/cosmos';
+import { createRefreshToken } from '../core/auth/createRefreshToken';
+import { createAccessToken } from '../core/auth/createAccessToken';
+
+interface Login {
+  email: string;
+  password: string;
+}
 
 export default async function (context: Context, req: HttpRequest) {
-  context.res = await login(req.body);
+  const body = req.body as Login;
+
+  if (
+    typeof body.email !== 'string' ||
+    !body.email.includes('@') ||
+    typeof body.password !== 'string'
+  ) {
+    return responses.INVALID_LOGIN();
+  }
+
+  const { users } = await getDatabase();
+
+  try {
+    const { resources: [user] } = await users.items.query<User & Resource>(
+      `SELECT * FROM c`,
+      { partitionKey: body.email.toLowerCase() },
+    ).fetchAll();
+
+    if (!user) {
+      return responses.INVALID_LOGIN();
+    }
+
+    if (!await bcrypt.compare(body.password, user.password)) {
+      return responses.INVALID_LOGIN();
+    }
+
+    const [refreshToken, accessToken] = await Promise.all([
+      createRefreshToken(user.id),
+      createAccessToken(user.id),
+    ]);
+
+    return responses.LOGGED_IN({
+      user: withoutResource(user),
+      authorization: {
+        refreshToken,
+        accessToken,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+
+    switch (err.code) {
+      default: {
+        return responses.FAILED_TO_LOGIN();
+      }
+    }
+  }
 };
