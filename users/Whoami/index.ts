2815e9a18045edd27b41eeebb213128dac7dedf0
Jacob Marshall <github@jacob.dev>
2021-07-05 10:54:52 +1200

    Remove whoami route

--- users/Whoami/index.ts
+++
@@ -1,6 +0,0 @@
-import { Context, HttpRequest } from '@azure/functions';
-import { whoami } from '../core/functions/whoami';
-
-export default async function (context: Context, req: HttpRequest) {
-  context.res = await whoami(req.headers.authorization);
-};
