ebc84536b854d930ee65e7c567cf5d5415440476
Jacob Marshall <github@jacob.dev>
2021-07-08 19:40:47 +1200

    Add more sample products

--- stores/core/data/sampleProducts.ts
+++ stores/core/data/sampleProducts.ts
@@ -31,8 +31,32 @@ export const sampleProducts: Product[] = [
       '/sample-product-images/clearwipe-lens-cleaner.webp',
     ],
     barcode: {
-      format: 'upc_e',
-      code: '12181292',
+      format: 'ean_13',
+      code: '4987072080993',
+    },
+  },
+  {
+    name: 'Harry Potter: Spells and Charms Ruled Journal',
+    description: 'A truly magical Harry Potter ruled journal.',
+    price: 11.50,
+    images: [
+      '/sample-product-images/harry-potter-spells-and-charms-ruled-journal.jpeg',
+    ],
+    barcode: {
+      format: 'ean_13',
+      code: '9401063002443',
+    },
+  },
+  {
+    name: 'Einstein Collection Metal Puzzles',
+    description: `Formed of the symbols of Einstein's ground-breaking E=mc2 equation, see if you can separate the two pieces of each metal puzzle and then reattach them Developed in 1905, Einstein's famous theory...`,
+    price: 19.99,
+    images: [
+      '/sample-product-images/einstein-collection-metal-puzzles.jpeg',
+    ],
+    barcode: {
+      format: 'ean_13',
+      code: '5060506531397',
     },
   },
 ];
--- stores/core/models/Product.ts
+++ stores/core/models/Product.ts
@@ -8,5 +8,5 @@ export interface Product {
 
 export interface ProductBarcode {
   code: string;
-  format: 'code_128' | 'ean' | 'ean_8' | 'code_39' | 'code_39_vin' | 'codabar' | 'upc' | 'upc_e' | 'i2of5' | '2of5' | 'code_93';
+  format: 'code_128' | 'ean' | 'ean_8' | 'code_39' | 'code_39_vin' | 'codabar' | 'upc' | 'upc_e' | 'i2of5' | '2of5' | 'code_93' | 'ean_13';
 }
